6e14046269c22045fc9e181528cc4c5ed98c06e6
Kevin Palma <kapalma05@gmil.com>
2021-11-04 22:31:31 -0600

    Se añadio la funcionalidad de collapse al menu de 1024px

--- link/core/static/js/link.js
+++ link/core/static/js/link.js
@@ -1,7 +1,12 @@
 var hamburguer = document.querySelector('.btn-hamburguer')
 var menu = document.querySelector('.menu')
+var collapseBtn = document.querySelector('.collapse-button')
 
 hamburguer.addEventListener('click', () => {
     hamburguer.classList.toggle('open-menu')
     menu.classList.toggle('open-menu')
+})
+
+collapseBtn.addEventListener('click', () => {
+    menu.classList.toggle('collapse-menu')
 })
