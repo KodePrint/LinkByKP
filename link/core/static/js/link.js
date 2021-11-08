13afdd516e0168fda961ad7c70f7730a633fdba4
Kevin Palma <kpalmar@miumg.edu.gt>
2021-11-08 09:26:37 -0600

    Se creo el listado de preformas con datatables

--- link/core/static/js/link.js
+++ link/core/static/js/link.js
@@ -1,12 +1,23 @@
 var hamburguer = document.querySelector('.btn-hamburguer')
 var menu = document.querySelector('.menu')
 var collapseBtn = document.querySelector('.collapse-button')
+var barState = localStorage.getItem('bar')
 
 hamburguer.addEventListener('click', () => {
     hamburguer.classList.toggle('open-menu')
     menu.classList.toggle('open-menu')
 })
 
+if (barState == 'collapse') {
+    menu.classList.add('collapse-menu')
+} else {
+    menu.classList.remove('collapse-menu')
+
+}
+
 collapseBtn.addEventListener('click', () => {
+    let state
     menu.classList.toggle('collapse-menu')
+    state = menu.classList.contains('collapse-menu') ? 'collapse' : 'expanded'
+    localStorage.setItem('bar', state)
 })
