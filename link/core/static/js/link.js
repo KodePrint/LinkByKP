73718c82121ca15b0eb02f583483ff570ad5f307
Kevin Palma <kpalmar@miumg.edu.gt>
2021-11-08 12:42:47 -0600

    Se dio funcionalidad al menu lateral, asi como al boton de colapsar el mismo, ajustando el contenido a este

--- link/core/static/js/link.js
+++ link/core/static/js/link.js
@@ -1,5 +1,6 @@
 var hamburguer = document.querySelector('.hamburger-menu')
 var menu = document.querySelector('.menu')
+var content = document.querySelector('.content')
 var collapseBtn = document.querySelector('.collapse-button')
 var barState = localStorage.getItem('bar')
 
@@ -10,14 +11,17 @@ hamburguer.addEventListener('click', () => {
 
 if (barState == 'collapse') {
     menu.classList.add('collapse-menu')
+    content.classList.add('collapse-menu')
 } else {
     menu.classList.remove('collapse-menu')
+    content.classList.remove('collapse-menu')
 
 }
 
 collapseBtn.addEventListener('click', () => {
     let state
     menu.classList.toggle('collapse-menu')
+    content.classList.toggle('collapse-menu')
     state = menu.classList.contains('collapse-menu') ? 'collapse' : 'expanded'
     localStorage.setItem('bar', state)
 })
